86d62665508c1e2052138bd3cfdf654bf0503505
Geoffrey Lee <geoffrey0953@gmail.com>
2024-08-09 12:26:29 -0700

    Frontend Website Sample
    Sample Website for Movie Recommnder

--- csv_file_creation.py
+++ csv_file_creation.py
@@ -7,14 +7,18 @@ from pathlib import Path
 load_dotenv('api_key.env')
 tmdb_api_key = os.getenv('TMDB_API_KEY')
 
+# Only run once!
+
 detailed_movies = []
 
+# Grab page details from tmdb
 def get_page_details(page_number):
     tmdb_discover_url = f'https://api.themoviedb.org/3/discover/movie?api_key={tmdb_api_key}&page={page_number}'
     response = requests.get(tmdb_discover_url)
     if response.status_code == 200:
         return response.json().get('results', [])
 
+# Add a comment to this
 def get_movie_details(page_number):
     movies = get_page_details(page_number)
     for movie in movies:
@@ -24,9 +28,11 @@ def get_movie_details(page_number):
         if response.status_code == 200:
             detailed_movies.append(response.json())
 
-for page in range(1,2):
+# Append each page and each movie from the page to a list
+for page in range(1,501):
     get_movie_details(page)
 
+# Create .csv file
 df = pd.DataFrame(detailed_movies)
 df.to_csv('detailed_movies.csv', index=False)
 
