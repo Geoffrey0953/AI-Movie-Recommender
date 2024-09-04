d8d3927a278bf3d39b3af0b635ad66b495285c0f
Geoffrey Lee <geoffrey0953@gmail.com>
2024-09-04 16:59:04 -0700

    fixed typo

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -151,4 +151,4 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWS_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = True
