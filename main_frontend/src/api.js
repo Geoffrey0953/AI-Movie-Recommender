9c472fd263deafa0ba128c03c2120402448943fa
Geoffrey Lee <geoffrey0953@gmail.com>
2024-09-04 16:15:53 -0700

    add backend url?

--- main_frontend/src/api.js
+++ main_frontend/src/api.js
@@ -1,9 +1,11 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
+const apiURL = "https://1861532d-e0b4-4bf5-ac48-8d07fc1e3a9c-dev.e1-us-cdp-2.choreoapis.dev/filmbertbackend/backend/v1.0"
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-})
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+});
 
 api.interceptors.request.use(
     (config) => {
