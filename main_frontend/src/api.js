6cf28ffcd7a08b9d8ac5345977cc89eae2c54e37
Geoffrey Lee <geoffrey0953@gmail.com>
2024-09-04 16:22:09 -0700

    fixed typo

--- main_frontend/src/api.js
+++ main_frontend/src/api.js
@@ -4,7 +4,7 @@ import { ACCESS_TOKEN } from "./constants"
 const apiURL = "https://1861532d-e0b4-4bf5-ac48-8d07fc1e3a9c-dev.e1-us-cdp-2.choreoapis.dev/filmbertbackend/backend/v1.0"
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiURL,
 });
 
 api.interceptors.request.use(
